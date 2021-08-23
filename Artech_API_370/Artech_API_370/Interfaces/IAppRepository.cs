d7b9d4824e16bf30960cc136371a2cb9997da893
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-23 21:40:24 +0200

    Add Interface

---
+++ Artech_API_370/Artech_API_370/Interfaces/IAppRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Interfaces
+{
+    public interface IAppRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(long id);
+        void Add(TEntity entity);
+
+        void Update(TEntity dbEntity, TEntity entity);
+        void Delete(TEntity entity);
+
+
+
+    }
+}
---
+++ Artech_API_370/Artech_API_370/Interfaces/IIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Interfaces
+{
+    public interface IIdentifier<TEntity> where TEntity : class
+    {
+        TEntity getUser(string UserName);
+    }
+}
