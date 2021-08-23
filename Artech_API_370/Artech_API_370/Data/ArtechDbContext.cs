e998b34482e5d2c5579fb897f5ed7ce69078e464
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-23 21:36:09 +0200

    Add Invitation Status and Art Class Type to Data and Dto

---
+++ Artech_API_370/Artech_API_370/Data/ArtechDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Entities.Artists;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Data
+{
+    public class ArtechDbContext : DbContext
+    {
+        public ArtechDbContext(DbContextOptions<ArtechDbContext> options) : base(options)
+        {
+
+
+        }
+
+        // Art Classes
+        public DbSet<ArtClassType> ArtClassTypes { get; set; }
+
+        // Artist 
+        public DbSet<InvitationStatus> InvitationStatus { get; set; }
+
+    }
+}
---
+++ Artech_API_370/Artech_API_370/Dtos/ArtistsDtos/InvitationStatusDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Dtos.ArtistsDtos
+{
+    public class InvitationStatusDto
+    {
+        public int InvitationStatusID { get; set; }
+        public string InvitationStatusDescription { get; set; }
+    }
+}
