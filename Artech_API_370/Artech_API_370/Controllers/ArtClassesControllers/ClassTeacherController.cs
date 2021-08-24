9a117d8a85707e2f4109b9ee1fdfbf410b66baf6
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-24 19:01:32 +0200

    Class Teacher Repo and Controller

---
+++ Artech_API_370/Artech_API_370/Controllers/ArtClassesControllers/ClassTeacherController.cs
@@ -0,0 +1,96 @@
+﻿using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Controllers.ArtClassesControllers
+{
+    [Route("api/ClassTeacher")]
+    [ApiController]
+    public class ClassTeacherController : ControllerBase
+    {
+        private readonly IAppRepository<ClassTeacher> _appRepository;
+
+        public ClassTeacherController(IAppRepository<ClassTeacher> appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        // GET: api/ClassTeacher
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<ClassTeacher> classTeachers = _appRepository.GetAll();
+
+            return Ok(classTeachers);
+        }
+
+        // GET: api/ClassTeacher/{id}
+
+        [HttpGet("{id}", Name = "GetClassTeacher")]
+        public IActionResult Get(long id)
+        {
+            ClassTeacher classTeacher = _appRepository.Get(id);
+
+
+            if (classTeacher == null)
+            {
+                return NotFound("Requested Class Teacher does not exist.");
+            }
+
+            return Ok(classTeacher);
+        }
+
+        // GET: api/Create
+        [HttpPost]
+        public IActionResult Post([FromBody] ClassTeacher classTeacher)
+        {
+            if (classTeacher == null)
+            {
+                return BadRequest("Class Teacher is null.");
+            }
+            _appRepository.Add(classTeacher);
+            return CreatedAtRoute(
+                  "GetClassTeacher",
+                  new { Id = classTeacher.ClassTeacherID },
+                  classTeacher);
+        }
+
+        // PUT: api/ClassTeacher/5
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] ClassTeacher classTeacher)
+        {
+            if (classTeacher == null)
+            {
+                return BadRequest("Class Teacher is null.");
+            }
+            ClassTeacher classTeacherToUpdate = _appRepository.Get(id);
+            if (classTeacherToUpdate == null)
+            {
+                return NotFound("The Class Teacher record couldn't be found.");
+            }
+            _appRepository.Update(classTeacherToUpdate, classTeacher);
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/ClassTeacher/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            ClassTeacher classTeacher = _appRepository.Get(id);
+            if (classTeacher == null)
+            {
+                return NotFound("The class teacher does not exist.");
+            }
+            _appRepository.Delete(classTeacher);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Artech_API_370/Artech_API_370/Repository/ArtClassesRepositories/ClassTeacherRepository.cs
@@ -0,0 +1,52 @@
+﻿using Artech_API_370.Data;
+using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Repository.ArtClassesRepositories
+{
+    public class ClassTeacherRepository : IAppRepository<ClassTeacher>
+    {
+        readonly ArtechDbContext _artechDb;
+
+
+        public ClassTeacherRepository(ArtechDbContext artechDb)
+        {
+            _artechDb = artechDb;
+        }
+
+        public void Add(ClassTeacher classTeacher)
+        {
+            _artechDb.ClassTeacher.Add(classTeacher);
+            _artechDb.SaveChanges();
+        }
+
+        public void Delete(ClassTeacher classTeacher)
+        {
+            _artechDb.ClassTeacher.Remove(classTeacher);
+            _artechDb.SaveChanges();
+        }
+
+        public ClassTeacher Get(long id)
+        {
+            return _artechDb.ClassTeacher.FirstOrDefault(u => u.ClassTeacherID == id);
+        }
+
+        public IEnumerable<ClassTeacher> GetAll()
+        {
+            return _artechDb.ClassTeacher.ToList();
+        }
+
+        public void Update(ClassTeacher classTeacher, ClassTeacher entity)
+        {
+            classTeacher.TeacherName = entity.TeacherName;
+            classTeacher.TeacherSurname = entity.TeacherSurname;
+            classTeacher.TeacherEmail = entity.TeacherEmail;
+            classTeacher.TeacherPhoneNumber = entity.TeacherPhoneNumber;
+            _artechDb.SaveChanges();
+        }
+    }
+}
