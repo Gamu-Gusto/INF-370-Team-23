e4b53f9fa529b07912c991c0f1190b164321ee6e
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-25 16:03:48 +0200

    Art Class Repo and Controller

---
+++ Artech_API_370/Artech_API_370/Controllers/ArtClassesControllers/ArtClassController.cs
@@ -0,0 +1,96 @@
+﻿using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Controllers.ArtClassesControllers
+{
+    [Route("api/ArtClass")]
+    [ApiController]
+    public class ArtClassController : ControllerBase
+    {
+        private readonly IAppRepository<ArtClass> _appRepository;
+
+        public ArtClassController(IAppRepository<ArtClass> appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        // GET: api/ArtClass
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<ArtClass> artClasses = _appRepository.GetAll();
+
+            return Ok(artClasses);
+        }
+
+        // GET: api/User/{id}
+
+        [HttpGet("{id}", Name = "GetArtClass")]
+        public IActionResult Get(long id)
+        {
+            ArtClass artClass = _appRepository.Get(id);
+
+
+            if (artClass == null)
+            {
+                return NotFound("Requested Art Class does not exist.");
+            }
+
+            return Ok(artClass);
+        }
+
+        // GET: api/Create
+        [HttpPost]
+        public IActionResult Post([FromBody] ArtClass artClass)
+        {
+            if (artClass == null)
+            {
+                return BadRequest("Art Class is null.");
+            }
+            _appRepository.Add(artClass);
+            return CreatedAtRoute(
+                  "GetArtClass",
+                  new { Id = artClass.ArtClassID },
+                  artClass);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] ArtClass artClass)
+        {
+            if (artClass == null)
+            {
+                return BadRequest("Art Class is null.");
+            }
+            ArtClass artClassToUpdate = _appRepository.Get(id);
+            if (artClassToUpdate == null)
+            {
+                return NotFound("The Art Class record couldn't be found.");
+            }
+            _appRepository.Update(artClassToUpdate, artClass);
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            ArtClass artClass = _appRepository.Get(id);
+            if (artClass == null)
+            {
+                return NotFound("The Art Class does not exist.");
+            }
+            _appRepository.Delete(artClass);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Artech_API_370/Artech_API_370/Repository/ArtClassesRepositories/ArtClassRepository.cs
@@ -0,0 +1,58 @@
+﻿using Artech_API_370.Data;
+using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Repository.ArtClassesRepositories
+{
+    public class ArtClassRepository : IAppRepository<ArtClass>
+    {
+        readonly ArtechDbContext _artechDb;
+
+
+        public ArtClassRepository(ArtechDbContext artechDb)
+        {
+            _artechDb = artechDb;
+        }
+
+        public void Add(ArtClass artClass)
+        {
+            _artechDb.ArtClasse.Add(artClass);
+            _artechDb.SaveChanges();
+        }
+
+        public void Delete(ArtClass artClass)
+        {
+            _artechDb.ArtClasse.Remove(artClass);
+            _artechDb.SaveChanges();
+
+        }
+
+        public ArtClass Get(long id)
+        {
+            return _artechDb.ArtClasse.FirstOrDefault(u => u.ArtClassID == id);
+        }
+
+        public IEnumerable<ArtClass> GetAll()
+        {
+            return _artechDb.ArtClasse.ToList();
+        }
+
+        public void Update(ArtClass artClass, ArtClass entity)
+        {
+            artClass.ArtClassName = entity.ArtClassName;
+            artClass.ArtClassDescription = entity.ArtClassDescription;
+            artClass.ArtClassStartDate = entity.ArtClassStartDate;
+            artClass.ArtClassEndDate = entity.ArtClassEndDate;
+            artClass.ArtClassStartTime = entity.ArtClassStartTime;
+            artClass.ArtClassEndTime = entity.ArtClassEndTime;
+            artClass.ClassLimit = entity.ClassLimit;
+            artClass.RefundDayLimit = entity.RefundDayLimit;
+            artClass.ClassPrice = entity.ClassPrice;
+            _artechDb.SaveChanges();
+        }
+    }
+}
