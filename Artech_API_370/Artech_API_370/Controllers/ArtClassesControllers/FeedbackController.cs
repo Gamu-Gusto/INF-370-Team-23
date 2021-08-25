6f6a97c815b163e330c4ec2b4d575c9b5349df23
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-25 16:08:17 +0200

    Feedback Repo and Controller

---
+++ Artech_API_370/Artech_API_370/Controllers/ArtClassesControllers/FeedbackController.cs
@@ -0,0 +1,96 @@
+﻿using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Controllers.ArtClassesControllers
+{
+    [Route("api/Feedback")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IAppRepository<Feedback> _appRepository;
+
+        public FeedbackController(IAppRepository<Feedback> appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        // GET: api/Feedback
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Feedback> feedbacks = _appRepository.GetAll();
+
+            return Ok(feedbacks);
+        }
+
+        // GET: api/Feedback/{id}
+
+        [HttpGet("{id}", Name = "GetFeedback")]
+        public IActionResult Get(long id)
+        {
+            Feedback feedback = _appRepository.Get(id);
+
+
+            if (feedback == null)
+            {
+                return NotFound("Requested Feedback does not exist.");
+            }
+
+            return Ok(feedback);
+        }
+
+        // GET: api/Create
+        [HttpPost]
+        public IActionResult Post([FromBody] Feedback feedback)
+        {
+            if (feedback == null)
+            {
+                return BadRequest("Feedback is null.");
+            }
+            _appRepository.Add(feedback);
+            return CreatedAtRoute(
+                  "GetFeedback",
+                  new { Id = feedback.FeedbackID },
+                  feedback);
+        }
+
+        // PUT: api/Feedback/5
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] Feedback feedback)
+        {
+            if (feedback == null)
+            {
+                return BadRequest("Feedback is null.");
+            }
+            Feedback feedbackToUpdate = _appRepository.Get(id);
+            if (feedbackToUpdate == null)
+            {
+                return NotFound("The Feedback record couldn't be found.");
+            }
+            _appRepository.Update(feedbackToUpdate, feedback);
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/Feedback/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            Feedback feedback = _appRepository.Get(id);
+            if (feedback == null)
+            {
+                return NotFound("The Feedback does not exist.");
+            }
+            _appRepository.Delete(feedback);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Artech_API_370/Artech_API_370/Repository/ArtClassesRepositories/FeedbackRepository.cs
@@ -0,0 +1,52 @@
+﻿using Artech_API_370.Data;
+using Artech_API_370.Entities.ArtClasses;
+using Artech_API_370.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Repository.ArtClassesRepositories
+{
+    public class FeedbackRepository : IAppRepository<Feedback>
+    {
+        readonly ArtechDbContext _artechDb;
+
+
+        public FeedbackRepository(ArtechDbContext artechDb)
+        {
+            _artechDb = artechDb;
+        }
+
+        public void Add(Feedback feedback)
+        {
+            _artechDb.Feedback.Add(feedback);
+            _artechDb.SaveChanges();
+        }
+
+        public void Delete(Feedback feedback)
+        {
+            _artechDb.Feedback.Remove(feedback);
+            _artechDb.SaveChanges();
+        }
+
+        public Feedback Get(long id)
+        {
+            return _artechDb.Feedback.FirstOrDefault(u => u.FeedbackID == id);
+        }
+
+        public IEnumerable<Feedback> GetAll()
+        {
+            return _artechDb.Feedback.ToList();
+        }
+
+        public void Update(Feedback feedback, Feedback entity)
+        {
+            feedback.FeedbackComment = entity.FeedbackComment;
+            feedback.TeacherRating = entity.TeacherRating;
+            feedback.DifficultyRating = entity.DifficultyRating;
+            feedback.OverallRating = entity.OverallRating;
+            _artechDb.SaveChanges();
+        }
+    }
+}
