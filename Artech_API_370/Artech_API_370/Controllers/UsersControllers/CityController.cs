71c8b7ec4c501df7723a5251a78e4edefc361575
u18201696 <50372208+u18201696@users.noreply.github.com>
2021-08-27 00:30:28 +0200

    Continuation of User Controllers

---
+++ Artech_API_370/Artech_API_370/Controllers/UsersControllers/CityController.cs
@@ -0,0 +1,97 @@
+﻿using Artech_API_370.Entities.Users;
+using Artech_API_370.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artech_API_370.Controllers.UsersControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : Controller
+    {
+        private readonly IAppRepository<City> _appRepository;
+
+        public CityController(IAppRepository<City> appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        // GET: api/City
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<City> cities = _appRepository.GetAll();
+
+            return Ok(cities);
+        }
+
+        // GET: api/City/{id}
+
+        [HttpGet("{id}", Name = "GetCity")]
+        public IActionResult Get(long id)
+        {
+            City city = _appRepository.Get(id);
+
+
+            if (city == null)
+            {
+                return NotFound("Requested City does not exist.");
+            }
+
+            return Ok(city);
+        }
+
+        // GET: api/City
+        [HttpPost]
+        public IActionResult Post([FromBody] City city)
+        {
+            if (city == null)
+            {
+                return BadRequest("City is null.");
+            }
+            _appRepository.Add(city);
+            return CreatedAtRoute(
+                  "GetCity",
+                  new { Id = city.CityID },
+                  city);
+        }
+
+        // PUT: api/City/{5}
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] City city)
+        {
+            if (city == null)
+            {
+                return BadRequest("City is null.");
+            }
+
+            City cityToUpdate = _appRepository.Get(id);
+            if (cityToUpdate == null)
+            {
+                return NotFound("The City does not exist.");
+            }
+            _appRepository.Update(cityToUpdate, city);
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/City/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            City city = _appRepository.Get(id);
+            if (city == null)
+            {
+                return NotFound("The city does not exist.");
+            }
+            _appRepository.Delete(city);
+
+            return NoContent();
+        }
+    }
+}
