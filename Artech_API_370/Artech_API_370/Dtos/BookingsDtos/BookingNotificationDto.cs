019c0b3fd4fd31b05863983dca8d3283fb76912c
DeanBotha1 <88797178+DeanBotha1@users.noreply.github.com>
2021-08-26 15:06:17 +0200

    Create BookingNotificationDto.cs

---
+++ Artech_API_370/Artech_API_370/Dtos/BookingsDtos/BookingNotificationDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BinaryBrainsAPI.Dtos.BookingsDtos
+{
+    public class BookingNotificationDto
+    {
+        public int BookingNotificationID { get; set; }
+        public string BookNotificationDescription { get; set; }
+    }
+}
